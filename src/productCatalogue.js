9ced21131ffa19f94cfcf8badc3367384ce617ab
yuxuan wang <2879296442@qq.com>
2020-10-15 15:03:44 +0100

    Tested remove and find product by id methods

--- src/productCatalogue.js
+++ src/productCatalogue.js
@@ -16,6 +16,15 @@ class Catalogue {
     }
     return false;
   }
-
+  removeProductById(id) {
+    const removedProduct = this.findProductById(id);
+    if (removedProduct) {
+      this.products = this.products.filter(
+        (product) => product.id !== id 
+      );
+    }
+    return removedProduct;
+  }
 }
+
 module.exports = Catalogue;
